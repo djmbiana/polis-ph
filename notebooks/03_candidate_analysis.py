304bbe3d7eb01780316f200969e70b873ef5ff6d
Derrick James Biana <djmbiana@gmail.com>
2026-05-25 20:11:32 +0800

    Added vote share per region for each candidate

--- notebooks/03_candidate_analysis.py
+++ notebooks/03_candidate_analysis.py
@@ -40,10 +40,35 @@ def _(df):
     return sen_votes_region, top12
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Candidates per region
+    - Candidates seem to score higher from their home region
+        - To test this finding, I decided to isolate Dela Rosa, Bato who is from Region XI and compare his vote share between all the different regions
+        - Region XI has about 66.1% more compared to the average vote share of other regions, this finding shows that our hypothesis is true.
+        - I tested it further by isolating Go, Bong (Region XI) and Pangilinan, Kiko (NCR) and they followed the same results.
+    """)
+    return
+
+
 @app.cell
 def _(sen_votes_region, top12):
     top12_names = top12["candidate"].tolist()
-    sen_votes_region[top12_names].sort_values(by=top12_names[0], ascending=False)
+    top12_regions = sen_votes_region[top12_names].sort_values(by=top12_names[0], ascending=False)
+    top12_regions
+    return (top12_regions,)
+
+
+@app.cell
+def _(df, top12_regions):
+    # change the candidate name for the candidate you want to examine
+    candidate = "22. DELA ROSA, BATO (PDPLBN)"
+    bato = top12_regions[candidate].to_frame()
+    actual_vote_sum = df.groupby("region")["actualVoters"].sum()
+    joined_df = bato.join(actual_vote_sum)
+    joined_df["voteShare"] = (joined_df[candidate] / joined_df["actualVoters"] * 100).round(2)
+    joined_df
     return
 
 
