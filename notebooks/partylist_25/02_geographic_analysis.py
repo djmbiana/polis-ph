06c8539e1cf6f786c40a74e2ac7a9b28bd72e545
Derrick James Biana <djmbiana@gmail.com>
2026-05-26 11:55:10 +0800

    For senate_25 geographic analysis, i fixed the formatting. For partylist_25, i completed it. Both sources have similarities which means that data remains consistent between both datasets.

--- notebooks/partylist_25/02_geographic_analysis.py
+++ notebooks/partylist_25/02_geographic_analysis.py
@@ -8,7 +8,7 @@ app = marimo.App()
 def _():
     import marimo as mo
 
-    mo.md("# Senate Votes Geographic Analysis")
+    mo.md("# Partylist Votes Geographic Analysis")
     return (mo,)
 
 
@@ -18,21 +18,21 @@ def _():
     import seaborn as sns
     import matplotlib.pyplot as plt
 
-    df = pd.read_csv('../../datasets/senate25-final_updated.csv')
+    df = pd.read_csv('../../datasets/partylist25-final_updated.csv')
     return df, plt, sns
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    ## Registered Voters vs Actual Voters per region
+    ## Registered voters vs Actual voters per region
     - Registered voters are individuals who have registered under comelec for voting
     - Actual voters are those who actually voted, indicating actual turnouts during the election
-    - Region IV-A has the most registered voters
-    - Region VII has the highest turnout percentage, meaning that 86.51% of registered voters do end up voting
-    - OAV has a lot of registered voters with a turnout percentage of 18.37%. This could indicate that a lot of overseas Filipinos are able to register but are unable to make it to their respective embassies to vote.
-    - LAV has the least amount of registered voters with a turnout percentage of 52.07%. This is because these are special individuals (military, media, government officials, etc) who do not vote under their registered precinct, hence the low counts.
-    - Keep in mind, turnout percentage only reflects the proportion of registered voters who voted, not the overall voting-age population of each region
+    - Results are similar to the senate_25 dataset
+        - Region IV-A has the most registered voters
+        - Region VII has the highest turnout percentage
+        - OAV has a turnout percentage of 18.37%. This could indicate that a lot of overseas Filipinos are able to register but are unable to vote.
+    - LAV has a turnout percentage of 52.07%. This is because these are individuals who do not vote at their registered precinct
     """)
     return
 
@@ -48,12 +48,10 @@ def _(df):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    ### Turnout percentage
-    - The average turnout rate across all regions is 78.90%
-    - Most regions under Government jurisdiction have a turnout percentage of 80%+
-    - BARMM is an autonomous region with its own governance structure (Bangsamoro Government), which may contribute to its lower turnout of 77%
-    - OAV and LAV are outliers as well
-        - As mentioned, OAV remains an outlier as overseas filipinos may not be able to vote at their given embassy
+    ### Turnout Perecentage
+    - The average turnout rate across all regions is the same as senate_25 (78.90%)
+    - OAV and LAV are also outliers
+        - OAV is an outlier because OFWs may not be able to vote at their given embassy
         - LAV only accounts for special individuals who do not vote at their registered precinct.
     """)
     return
