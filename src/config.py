7f2de071e0d619481e609fe0f3a6bf06bcb42bdb
Derrick James Biana <djmbiana@gmail.com>
2026-05-28 19:58:00 +0800

    Created .env (though its in gitignore for safety) and config.py to load in the variables and info from .env

---
+++ src/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# setting root directory
+REPO_ROOT = Path(__file__).resolve().parent.parent
+
+# loading the .env file
+load_dotenv(REPO_ROOT / ".env")
+
+# POSTGRES information
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+RAW_DATA_PATH = os.getenv("RAW_DATA_PATH")
+DB_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
