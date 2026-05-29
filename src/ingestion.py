de2d8d72e4d337a95284598e61802880f49583d4
Derrick James Biana <djmbiana@gmail.com>
2026-05-29 19:30:26 +0800

    Fixed ingestion to return DF

--- src/ingestion.py
+++ src/ingestion.py
@@ -8,23 +8,25 @@ file_path_senate = con.RAW_DATA_PATH / "senate25-final_updated.csv"
 file_path_partylist = con.RAW_DATA_PATH / "partylist25-final_updated.csv"
 
 
-def load_senate_25(filepath: Path) -> None:
+def load_senate_25(filepath: Path) -> pd.DataFrame:
     """
     loads the senate_25 csv into raw.senate_25 table in postgresql.
     replaces the table if it already exists.
     """
     df = pd.read_csv(filepath)
     eng = create_engine(connect_url)
-
     df.to_sql("senate_25", eng, if_exists="replace", index=False, schema="raw")
 
+    return df
 
-def load_partylist_25(filepath: Path) -> None:
+
+def load_partylist_25(filepath: Path) -> pd.DataFrame:
     """
     loads the partylist_25 csv into raw.partylist_25 table in postgresql.
     replaces the table if it already exists.
     """
     df = pd.read_csv(filepath)
     eng = create_engine(connect_url)
-
     df.to_sql("partylist_25", eng, if_exists="replace", index=False, schema="raw")
+
+    return df
