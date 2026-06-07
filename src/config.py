08e6c225c67115854b9ce2d333e90b8a2adc8d32
Derrick James Biana <djmbiana@gmail.com>
2026-06-07 13:41:21 +0800

    Changed ingestion.py to load into mongo instead of postgres.
    This is to set mongodb as the raw data landing zone.

--- src/config.py
+++ src/config.py
@@ -10,10 +10,15 @@ REPO_ROOT = Path(__file__).resolve().parent.parent
 load_dotenv(REPO_ROOT / ".env")
 
 # POSTGRES information
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+DB_HOST = os.getenv("HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+
 RAW_DATA_PATH = REPO_ROOT / os.getenv("RAW_DATA_PATH", "datasets")
-DB_URL = f"postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DB_URL = f"postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{DB_HOST}:27017"
--- src/ingestion.py
+++ src/ingestion.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 
 import pandas as pd
-from sqlalchemy import create_engine
+from pymongo import MongoClient
 
 import config as con
 
-connect_url = con.DB_URL
 file_path_senate = con.RAW_DATA_PATH / "senate25-final_updated.csv"
 file_path_partylist = con.RAW_DATA_PATH / "partylist25-final_updated.csv"
 
@@ -16,9 +15,10 @@ def load_senate_25(filepath: Path) -> pd.DataFrame:
     replaces the table if it already exists.
     """
     df = pd.read_csv(filepath)
-    eng = create_engine(connect_url)
-    df.to_sql("senate_25", eng, if_exists="replace", index=False, schema="raw")
-
+    client = MongoClient(con.MONGO_URL)
+    db = client["polis"]
+    db["raw_senate_25"].insert_many(df.to_dict("records"))
+    client.close()
     return df
 
 
@@ -28,9 +28,10 @@ def load_partylist_25(filepath: Path) -> pd.DataFrame:
     replaces the table if it already exists.
     """
     df = pd.read_csv(filepath)
-    eng = create_engine(connect_url)
-    df.to_sql("partylist_25", eng, if_exists="replace", index=False, schema="raw")
-
+    client = MongoClient(con.MONGO_URL)
+    db = client["polis"]
+    db["raw_partylist_25"].insert_many(df.to_dict("records"))
+    client.close()
     return df
 
 
