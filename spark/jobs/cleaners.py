7f069d5ae0f3de2dd6a2ea22c2ec5ee3a20f1290
Derrick James Biana <djmbiana@gmail.com>
2026-06-08 12:26:23 +0800

    Added data validations to cleaners.py
    This will ensure that no bad data gets passed down the pipeline, will
    add more edge cases as i go along.

--- spark/jobs/cleaners.py
+++ spark/jobs/cleaners.py
@@ -1,9 +1,12 @@
 import re
 import sys
+from functools import reduce
 
 sys.path.insert(0, "/opt/spark/src")
-from pyspark.sql import SparkSession, DataFrame
+
+from pyspark.sql import Column, DataFrame, SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.types import IntegerType, LongType
 
 import config as con
 
@@ -31,6 +34,81 @@ df = (
 )
 
 
+# check for negatives
+def check_negatives(df: DataFrame) -> DataFrame:
+    """
+    Checks all numerical columns if they have negative numbers.
+    This is important because voting machines cannot recieve negative numbers (unless machine is broken or it was tampered with.)
+    """
+    numeric_cols = [
+        f.name
+        for f in df.schema.fields
+        if isinstance(f.dataType, (IntegerType, LongType))
+    ]
+    conditions = [F.col(f"`{c}`") < 0 for c in numeric_cols]
+    condition: Column = reduce(lambda a, b: a | b, conditions)
+    negative_rows = df.filter(condition)
+    count = negative_rows.count()
+    if count > 0:
+        raise ValueError(f"Found {count} rows with negative values, aborting job.")
+    print("Negative check passed: no negative values found")
+    return df
+
+
+# check of vote turnout is invalid
+def invalid_turnout(df: DataFrame) -> DataFrame:
+    """
+    Checks the count of actual_voters and registered_voters
+    actual_voters cannot exceed registered_voters as non-registered individuals cannot vote.
+    """
+    condition: Column = F.col("actualVoters") > F.col("registeredVoters")
+    invalid_rows = df.filter(condition)
+    count = invalid_rows.count()
+    if count > 0:
+        raise ValueError(
+            f"Found {count} rows where actual voters exceed registered voters, aborting job."
+        )
+    print("Vote turnout is valid: Actual voters and registered voters add up.")
+    return df
+
+
+def region_checking(df: DataFrame) -> DataFrame:
+    """
+    Checks all regions to ensure that there are no invalid regions (follows COMELEC's different regions)
+    """
+    expected_regions = {
+        "REGION I",
+        "REGION II",
+        "REGION III",
+        "REGION IV-A",
+        "REGION IV-B",
+        "REGION V",
+        "REGION VI",
+        "REGION VII",
+        "REGION VIII",
+        "REGION IX",
+        "REGION X",
+        "REGION XI",
+        "REGION XII",
+        "REGION XIII",
+        "NATIONAL CAPITAL REGION",
+        "CORDILLERA ADMINISTRATIVE REGION",
+        "BARMM",
+        "NIR",
+        "OAV",
+        "LAV",
+    }
+    condition: Column = ~F.col("region").isin(expected_regions)
+    invalid_rows = df.filter(condition)
+    count = invalid_rows.count()
+    if count > 0:
+        raise ValueError(
+            f"Found {count} of rows which have regions that do not match COMELEC's election jurisdiction"
+        )
+    print("Regions are valid: All regions fit COMELECs data.")
+    return df
+
+
 # normalize columns
 def normalize_columns(df: DataFrame) -> DataFrame:
     """
@@ -54,10 +132,10 @@ def separate_df(df: DataFrame) -> tuple[DataFrame, DataFrame, DataFrame]:
     return oav, lav, domestic
 
 
+check_negatives(df)
+invalid_turnout(df)
+region_checking(df)
 df = normalize_columns(df)
-
-# prints schema so the user is able to see which database was passed
-# df.printSchema()
 
 oav, lav, domestic = separate_df(df)
 
